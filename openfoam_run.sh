6aa43ab3da9653486dcf0d707cdd30010364ad36
root <root@ip-10-0-100-193.us-west-2.compute.internal>
2017-10-03 08:56:26 +0000

    modified openfoam_run.sh

--- openfoam_run.sh
+++ openfoam_run.sh
@@ -13,9 +13,14 @@ mkdir -p $FOAM_RUN
 cd $FOAM_RUN
 cp -r $FOAM_TUTORIALS/incompressible/simpleFoam/pitzDaily .
 cd pitzDaily
+rm ./0/U
+cp /root/U_template ./0/U
+sed -i -e "s/\${xVelocity}/$1/g" 0/U
+sed -i -e "s/\${yVelocity}/$2/g" 0/U
+sed -i -e "s/\${zVelocity}/$3/g" 0/U
 blockMesh
 simpleFoam
 
 # upload U to S3
 TIMESTAMP=`date +%Y%m%d%H%M%S%N`
-aws s3 cp ./298/U s3://$BUCKETNAME/$TIMESTAMP
+aws s3 cp ./ s3://$BUCKETNAME/$TIMESTAMP/ --recursive
